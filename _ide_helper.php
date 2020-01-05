7cd9c48712eb24e3e4dfc7f669db01d78c98e53b
Lukáš Neuschl <lukas@lumi-wd.com>
2020-01-05 12:11:57 +0100

    Puts PHP down to 7.3.

--- _ide_helper.php
+++ _ide_helper.php
@@ -3,7 +3,7 @@
 
 /**
  * A helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 6.9.0 on 2020-01-03 09:42:31.
+ * Generated for Laravel 6.9.0 on 2020-01-05 12:11:37.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
